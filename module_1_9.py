d74de26c14eb91ae09c764c7aaa3a4fd3b4ed599
SSasha88 <d1rus1853@gmail.com>
2024-11-12 21:12:59 +0300

    Усложнил себе задачу

---
+++ module_1_9.py
@@ -0,0 +1,10 @@
+name = input("Ваше имя ")
+immutable_var = 1,2, 3, 'module_1'
+print('immutable_var', immutable_var)
+print(type(immutable_var))
+mutable_list = [1,2,3],4,5, 'module_1'
+print('mutable_list', mutable_list)
+print(type(mutable_list))
+mutable_list [0][1]= name
+print(mutable_list)
+print(type(mutable_list))
